3beec6a16553b76890c6ab84b4c658fd7e254433
soomin <s2305@e-mirim.hs.kr>
2024-11-17 19:56:05 +0900

    [feat] 포트 4000으로 변경

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();  // dotenv 패키지 로드
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.json());  // JSON 파싱을 위한 미들웨어
 
